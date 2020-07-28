6aa137ab41398b6bc09c81357dba1ad7bb7e2788
notabene <notabene1991@yandex.ru>
2020-07-28 15:28:24 +0300

    setup some slider settings

--- src/vendor/swiper.js
+++ src/vendor/swiper.js
@@ -4,19 +4,38 @@ const slider = document.querySelector('.swiper-container');
 Swiper.use([Navigation, Pagination]);
 
 let mySwiper = new Swiper(slider, {
-    direction: 'horizontal',
-    loop: true,
-    slidesPerView: '3',
-    spaceBetween: 16,
-    keyboard: true,
-
-    pagination: {
-        el: '.swiper-pagination',
-        type: 'bullets',
-        clickable: true,
+  direction: 'horizontal',
+  loop: true,
+  slidesPerView: '1',
+  spaceBetween: 20,
+  keyboard: true,
+  centeredSlides: true, 
+  centeredSlidesBounds: true, 
+  pagination: {
+    el: '.swiper-pagination',
+    type: 'bullets',
+    clickable: true,
+  },
+  navigation: {
+    nextEl: '.swiper-button-next',
+    prevEl: '.swiper-button-prev',
+  },
+  breakpoints: {
+    320: {
+      slidesPerView: 1,
+      spaceBetween: 20
     },
-    navigation: {
-        nextEl: '.swiper-button-next',
-        prevEl: '.swiper-button-prev',
+    480: {
+      slidesPerView: 1,
+      spaceBetween: 20
     },
+    1024: {
+      slidesPerView: 2,
+      spaceBetween: 20
+    },
+    1440: {
+      slidesPerView: 4,
+      spaceBetween: 16
+    }
+  }
 })
