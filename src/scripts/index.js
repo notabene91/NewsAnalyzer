25815159f7db680721936198696c55afe800f812
Александр Шабалин <notabene1991@yandex.ru>
2020-08-13 03:26:14 +0300

    some css hotfixes for mobiles

--- src/scripts/index.js
+++ src/scripts/index.js
@@ -16,61 +16,73 @@ import {
   cardsSection,
   cardsList,
   searchButton,
-  searchError
+  searchError,
+  showMoreBtn
 } from "./constants/Constants"
 
-
-// searchForm.addEventListener('submit', evt => {
-//   evt.preventDefault()
-// })
-
-function cardListWhenOpen() {
-  notFound.classList.remove('not-found_visible');
-  input.value = dataStorage.getItem('input');
-  searchButton.classList.add('search__button_inactive')
-  if (!localStorage.input) {
+(function () {
+  function cardListWhenOpen() {
+    notFound.classList.remove('not-found_visible');
+    input.value = dataStorage.getItem('input');
+    searchButton.classList.add('search__button_inactive')
+    if (!localStorage.input) {
+      searchButton.setAttribute('disabled', true);
+    }
+    if (localStorage.cards) {
+      cardsSection.classList.add('cards_visible');
+      newsCardList.render(sliceCards(dataStorage.parseItem('cards').articles)[0]);
+    }
+  }
+  function sliceCards(arr) {
+    const slicedArr = []
+    for (let i = 0; i < arr.length; i += 3) {
+      slicedArr.push(arr.slice(i, i + 3))
+    }
+    return slicedArr
+  }
+  function renderNewsCards() {
+    newsCardList.clear();
+    dataStorage.removeItem('cards');
     searchButton.setAttribute('disabled', true);
+    preloader.classList.add('preloader_visible');
+    newsApi.getNews(input.value)
+      .then((res) => {
+        dataStorage.setItem('input', input.value);
+        if (res.totalResults === 0) {
+          notFound.classList.add('not-found_visible');
+        }
+        else {
+          notFound.classList.remove('not-found_visible');
+          cardsSection.classList.add('cards_visible');
+          dataStorage.setItem('cards', JSON.stringify(res));
+          newsCardList.render(sliceCards(dataStorage.parseItem('cards').articles)[0]);
+        }
+      })
+      .catch(err => {
+        alert(`Ошибка: ${err}`)
+      })
+      .finally(() => {
+        preloader.classList.remove('preloader_visible')
+        searchButton.removeAttribute('disabled');
+      })
   }
-  if (localStorage.cards) {
-    cardsSection.classList.add('cards_visible');
-    newsCardList.render(dataStorage.parseItem('cards').articles);
-  }
-}
 
-function renderNewsCards() {
-  newsCardList.clear();
-  dataStorage.removeItem('cards');
-  searchButton.setAttribute('disabled', true);
-  preloader.classList.add('preloader_visible');
-  newsApi.getNews(input.value)
-    .then((res) => {
-      dataStorage.setItem('input', input.value);
-      if (res.totalResults === 0) {
-        notFound.classList.add('not-found_visible');
-      }
-      else {
-        notFound.classList.remove('not-found_visible');
-        cardsSection.classList.add('cards_visible');
-        dataStorage.setItem('cards', JSON.stringify(res));
-        newsCardList.render(dataStorage.parseItem('cards').articles);
-      }
-    })
-    .catch(err => {
-      alert(`Ошибка: ${err}`)
-    })
-    .finally(() => {
-      preloader.classList.remove('preloader_visible')
-      searchButton.removeAttribute('disabled');
-    })
-}
-
-const dataStorage = new DataStorage();
-const newsApi = new NewsApi(configNews);
-const addFunction = (card) => (new NewsCard(card, newsMarkup, formatDateforCard, findLinks).createCard());
-const newsCardList = new NewsCardList(cardsList, addFunction);
-const searchInput = new SearchInput(renderNewsCards, searchForm, input, searchButton, searchError)
-cardListWhenOpen();
-
-searchInput.setEventListeners()
-
-
+  const dataStorage = new DataStorage();
+  const newsApi = new NewsApi(configNews);
+  const addFunction = (card) => (new NewsCard(card, newsMarkup, formatDateforCard, findLinks).createCard());
+  const newsCardList = new NewsCardList(cardsList, addFunction);
+  const searchInput = new SearchInput(renderNewsCards, searchForm, input, searchButton, searchError)
+  let baseSlice = 0;
+  cardListWhenOpen();
+  searchInput.setEventListeners()
+  showMoreBtn.addEventListener('click', () => {
+    baseSlice++;
+    if (baseSlice < sliceCards((dataStorage.parseItem('cards').articles)).length - 1) {
+      newsCardList.render(sliceCards(dataStorage.parseItem('cards').articles)[baseSlice])
+    }
+    else {
+      showMoreBtn.setAttribute('style', 'display: none')
+      newsCardList.render(sliceCards(dataStorage.parseItem('cards').articles)[baseSlice])
+    }
+  })
+})()
